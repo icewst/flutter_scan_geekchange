0fdef3f896a0da069ad3106e6f19033ae560f86b
geekwang <geekwang@tencent.com>
2025-06-11 10:07:45 +0800

    xxx

--- android/src/main/java/com/chavesgu/scan/ScanPlugin.java
+++ android/src/main/java/com/chavesgu/scan/ScanPlugin.java
@@ -35,7 +35,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.embedding.engine.plugins.FlutterPlugin;
 
 import static android.content.Context.VIBRATOR_SERVICE;
 
